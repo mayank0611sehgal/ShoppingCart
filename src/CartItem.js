a7f3beb358fbe6f1533d707b25084088e5f5137d
Mayank <mayank0611sehgal@gmail.com>
2021-06-15 18:36:42 +0530

    Add increaseQuantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,47 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+    increaseQuantity = () => {
+        console.log(this.state);
+    }
     render(){
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={ styles.image } />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize:25 }} >Phone</div>
-                    <div style={{ color:'#777' }} >Rs 999</div>
-                    <div style={{ color:'#777' }} >Qty: 1</div>
+                    <div style={{ fontSize:25 }} >{title}</div>
+                    <div style={{ color:'#777' }} >Rs {price}</div>
+                    <div style={{ color:'#777' }} >Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img 
+                            alt="increase" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/3303/3303893.png" 
+                            onClick={ this.increaseQuantity }
+                        />
+                        <img 
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/2740/2740679.png" 
+                        />
+                        <img 
+                            alt="delete" 
+                            className="action-icons" 
+                            src="https://image.flaticon.com/icons/png/512/2907/2907762.png" 
+                        />
                     </div>
                 </div>
             </div>
