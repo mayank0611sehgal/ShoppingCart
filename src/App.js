a3f2417c17cdf160be07ace8028b3054a9a88e88
Mayank <mayank0611sehgal@gmail.com>
2021-06-16 22:29:56 +0530

    Added Products count in Navbar and moved state from Cart to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,90 @@
+import React from 'react';
 import Cart from'./Cart';
+import Navbar from'./Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Cart />
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state={
+        products: [
+            {
+                price: 99,
+                title: 'Watch',
+                qty: 1,
+                img: 'https://images.pexels.com/photos/705868/pexels-photo-705868.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                id: 1
+            },
+            {
+                price: 999,
+                title: 'Phone',
+                qty: 10,
+                img: 'https://images.pexels.com/photos/265658/pexels-photo-265658.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                id: 2
+            },
+            {
+                price: 9999,
+                title: 'Laptop',
+                qty: 4,
+                img: 'https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+                id: 3
+            },
+        ]
+    }
+  }
+  handleDecreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+      if(products[index].qty === 0){
+          return;
+      }
+      products[index].qty -= 1;
+      this.setState({
+          products:products
+      });
+
+  }
+  handleIncreaseQuantity = (product) => {
+      const {products} = this.state;
+      const index = products.indexOf(product);
+      products[index].qty += 1;
+      this.setState({
+          products:products
+      });
+
+  }
+  handleDeleteProduct = (id) => {
+      const {products} = this.state;
+      const items = products.filter((item) => (item.id !== id) );
+      this.setState({
+          products:items
+      });
+  }
+  getCartCount = () => {
+    const {products} = this.state;
+    let count=0;
+    products.forEach((product) => {
+      count += product.qty;
+    })
+    return count;
+  }
+
+  render() {
+    const {products} = this.state;
+    return (
+      <div className="App">
+        <Navbar 
+          count = {this.getCartCount()}
+        />
+        <Cart 
+          products = {products}
+          onIncreaseQuantity = {this.handleIncreaseQuantity}
+          onDecreaseQuantity = {this.handleDecreaseQuantity}
+          onDeleteProduct =  {this.handleDeleteProduct}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
